62ffd9ce52c7984beae43a2074735d563a99fbbc
lm0988576990-oss <lm0988576990@gmail.com>
2026-08-25 11:11:38 +0800

    Create product-card.tsx

---
+++ components/lm/product-card.tsx
@@ -0,0 +1,15 @@
+In simple terms, this file is like a digital storefront tag for each product. Here's what it does:
+
+This code creates a product card - like what you'd see when browsing items in an app. When someone looks at a product, this card shows:
+
+A picture of the item
+The name of the product
+The price, which can be in Pi or regular currency depending on the user
+Who's selling it
+Whether it's in stock or sold out
+A special gold badge if there's a discount for Pi users
+If an item is sold out, it clearly says "Out of Stock" right on the picture. And if someone's using Pi, they might see the original price crossed out with their special Pi price shown instead - a nice little perk!
+
+The card is also interactive - you can tap or click on it to see more details about that product. It's designed to be clean, easy to read, and look great on any screen size.
+
+Think of it as the perfect little shop window for each item in your marketplace!
